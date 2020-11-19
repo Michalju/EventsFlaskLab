adbd05e584c4a8e01ae8cc4d957b797f3be31b09
Michalju <juszkin@gmail.com>
2020-11-19 16:09:13 +0000

    completed

--- app/models/event_list.py
+++ app/models/event_list.py
@@ -2,9 +2,11 @@ from app.models.event import *
 
 
 
-event1 = Event("12th September 2020", "AC/DC", "111", "blue room", "AC/DC playing in blue room")
-event2 = Event("11th November 2020", "Metallica", "110", "grey room", "Metallica playing in grey room")
-events = [event1, event2]
+event1 = Event("12th September 2020", "AC/DC", "120000", "blue", "AC/DC playing in blue room",True)
+event2 = Event("11th November 2020", "Metallica", "110000", "orange", "Metallica playing in orange room", False)
+event3 = Event("12th December 2020", "Slipknot", "50000", "blue", "Slipknot playing in blue room",True)
+event4 = Event("11th January 2021", "Death", "110000", "black", "Death playing in grey room", False)
+events = [event1, event2, event3, event4]
 
 
 def add_new_event(event):
